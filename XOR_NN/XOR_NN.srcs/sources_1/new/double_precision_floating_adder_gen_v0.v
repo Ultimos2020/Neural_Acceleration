a465b95d2bd80a1b206a95f8d7a0dbc8ab233d75
Ultimos2020 <ratneshmohan31@gmail.com>
2025-04-26 10:36:25 +0530

    First Commit for XOR_NN project

---
+++ XOR_NN/XOR_NN.srcs/sources_1/new/double_precision_floating_adder_gen_v0.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.04.2025 10:26:12
+// Design Name: 
+// Module Name: double_precision_floating_adder_gen_v0
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module double_precision_floating_adder_gen_v0(
+    input [63:0] A,
+    input [63:0] B,
+    input reset_n,
+    input clk,
+    input [63:0] Sum
+    );
+endmodule
---
+++ XOR_NN/XOR_NN.srcs/sources_1/new/sigmoid_func.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.04.2025 21:57:21
+// Design Name: 
+// Module Name: sigmoid_func
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sigmoid_func(
+    input test,
+    output testo
+    );
+endmodule
